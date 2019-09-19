f29716632222abf391c23b8d4d7b886ef86cbe38
Reese Morgan <ramorgan212@gmail.com>
2019-09-19 15:07:10 -0400

    add a news w/ slug route and update to use symfony2 code style.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -12,12 +12,25 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController {
+class ArticleController
+{
 
-  /**
-   * @Route("/")
-   */
-  public  function homepage(){
-    return new Response('Hello World');
-  }
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('Hello World');
+    }
+
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(
+          sprintf('Future page to show the article: "%s"', $slug)
+        );
+    }
 }
