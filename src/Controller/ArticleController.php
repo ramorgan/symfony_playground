f1fb9bfb0b6bc62be43f37880d2e7f486f645853
Reese Morgan <ramorgan212@gmail.com>
2019-09-19 16:21:33 -0400

    add names to routes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -17,16 +17,16 @@ class ArticleController extends AbstractController
 {
 
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('Hello World');
+        return $this->render("article/homepage.html.twig");
     }
 
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
